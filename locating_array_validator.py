06775637495809416fe391ac87df710e0da97177
Dylan Green <dylan.green@westpoint.edu>
2022-12-24 02:00:19 -0600

    crossover and mutation additions

--- locating_array_validator.py
+++ locating_array_validator.py
@@ -3,6 +3,7 @@ import math
 import random
 import copy
 import time
+import os
 start_time = time.time()
 
 
@@ -67,8 +68,7 @@ def add_line(n,k,v,pop_size):                                                   
                 locating = True
                 return ar
         print('here')
-        n += 1                                                                 #adds a row if it makes it through the full iteration without generating a locating array
-#                                                                              #adds a row if it makes it through the full iteration without generating a locating array
+        n += 1                                                                 #adds a row if it makes it through the full iteration without generating a locating array                                                                            #adds a row if it makes it through the full iteration without generating a locating array
 
 def to_file(array):
     with open(f'output_{t}_{k}_{v}_{d}_{n}.txt', 'w') as file:
@@ -76,30 +76,66 @@ def to_file(array):
             file.write(' '.join([str(item) for item in row]))
             file.write('\n') 
 
+def mutations(val, child):
+    mutation = []   
+    temp_child = child[:]                                                       #creating a copy to avoid locality issues
+    for i in temp_child: print(f"{i}")
+    print("\n")
+    if val == 1:                                                                #modify a row
+        row_num = random.randint(0,n-1)
+        print(f"i will modify row {row_num}")
+        mutation = [random.randint(0,v-1) for i in range(0,k)]                  #generate a random v-1 value k amount of times
+        temp_child[row_num] = mutation
+        mutated_child = temp_child
+    if val == 2:
+        column_num = random.randint(0,k-1)
+        print(f"i will modify col {column_num}")
+        mutator = [random.randint(0,v-1) for i in range(0,n)]
+        for num in range(len(mutator)):
+            temp_child[num][column_num] = mutator[num]                        #modifies the value at {column_num} in each part of the 2d list 
+        mutated_child = temp_child
+    if val == 3:
+        rand_val = random.randint(0,v-1)
+        column_num = random.randint(0,k-1)     #picks random column in number of columns generated
+        row_num = random.randint(0,n-1)     #picks random int in number of rows generated
+        print(f"i will modify {row_num},{column_num}")
+        temp_child[row_num][column_num] = rand_val
+        mutated_child = temp_child
+    print("----------------------\n")
+    for i in mutated_child: print(f"{i}") 
+    print("\n")
+    return mutated_child
 
-def basic_genetic_algo(n,k,v,pop_size):
-    locating = False
-    list_algo_scope = generator(n,k,v,pop_size)
-    def breeding(list):                                                         #takes in list of arrays
-        parent1, parent2 = random.sample(list_algo_scope, k = 2)                #picks random array in list
-        child = []
-        mutation = []
+def crossover(parent1, parent2, val):               #we need crossover to take in two lists and a val (0,1) to determine if 1 or 2 point crossover
+    child = []
+    if val == 1:                                    #one point crossover
         idx = random.randint(0,n) 
         for elem in parent1[:idx]: 
             child.append(elem[:])
         for elem in parent2[idx:]:
             child.append(elem[:])
-        
-        #handeling simple mutation#
-        mutation = [random.randint(0,v-1) for i in range(0,k)]                  #generate a random v-1 value k amount of times
-        child[random.randint(0,n-1)] = mutation
+    if val == 2:                                   #two point crossover
+        idx1,idx2 = sorted(random.sample(range(0,n),2))             #ensure index 2 is always bigger than index 1
+        for elem in parent1[:idx1]: 
+            child.append(elem[:])
+        for elem in parent2[idx1:idx2]:
+            child.append(elem[:])
+        for elem in parent2[idx2:]:
+            child.append(elem[:])      
+    return child
+
+def basic_genetic_algo(n,k,v,pop_size):
+    locating = False
+    list_algo_scope = generator(n,k,v,pop_size)
+    def breeding(list):                                                         #takes in list of arrays
+        mut_num = random.randint(1,3)                                        #determining what number mutation to use
+        parent1, parent2 = random.sample(list_algo_scope, k = 2)                #picks random array in list
+        child = mutations(mut_num,crossover(parent1,parent2,random.randint(1,2)))
         list.append(child)
-    
     loop_time = time.time()
     while locating == False:
         breeding(list_algo_scope)
         fitness = locating_fitness(list_algo_scope[-1])
-
         if fitness == 1:            
             return list_algo_scope[-1]
         if time.time() - loop_time > 5.0:
